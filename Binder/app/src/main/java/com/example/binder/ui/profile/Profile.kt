75a263d11b71a4ae616fd5a877d67a33b971dba3
pertsevpv <pertsevpv@yandex.ru>
2024-04-02 12:47:21 +0300

    Rewrite Profile to fragment, add navigation view

---
+++ Binder/app/src/main/java/com/example/binder/ui/profile/Profile.kt
@@ -0,0 +1,9 @@
+package com.example.binder.ui.profile
+
+import com.example.binder.model.Book
+
+class Profile(
+    val name: String,
+    val personal: String,
+    val books: List<Book>
+)
---
+++ Binder/app/src/main/java/com/example/binder/ui/profile/ProfileFragment.kt
@@ -0,0 +1,88 @@
+package com.example.binder.ui.profile
+
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.binder.R
+import com.example.binder.databinding.FragmentProfileBinding
+import com.example.binder.model.Book
+import com.example.binder.profile.BookAdapter
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var bookRV: RecyclerView
+    private lateinit var bookAdapter: BookAdapter
+    private lateinit var profileName: EditText
+    private lateinit var profilePersonal: EditText
+
+    var profile = Profile("John Doe", "Hello!\nI'm John", listOf(
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024),
+        Book("Author", "Book", 2024)
+    ))
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        bookAdapter = BookAdapter()
+        bookRV = binding.profileRvBooks
+        profileName = binding.profileName
+        profilePersonal = binding.profilePersonalInfo
+
+        // Editable false
+        profileName.keyListener = null
+        profilePersonal.keyListener = null
+
+        updateUI()
+        return root
+    }
+
+    fun updateUI() {
+        profileName.setText(profile.name)
+        profilePersonal.setText(profile.personal)
+        bookAdapter.setData(profile.books)
+        bookRV.layoutManager = LinearLayoutManager(context)
+        bookRV.adapter = bookAdapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
