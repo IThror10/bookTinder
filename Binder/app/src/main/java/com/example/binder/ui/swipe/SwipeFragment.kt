bcb1cb08f174776c9b9bb2d3f80fc97a7db90f78
pertsevpv <pertsevpv@yandex.ru>
2024-04-03 11:19:48 +0300

    Minor swipe refactor

--- Binder/app/src/main/java/com/example/binder/ui/swipe/SwipeFragment.kt
+++ Binder/app/src/main/java/com/example/binder/ui/swipe/SwipeFragment.kt
@@ -3,6 +3,7 @@ package com.example.binder.ui.swipe
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
@@ -22,20 +23,20 @@ class SwipeFragment : Fragment() {
         _binding = FragmentSwipeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        return root
-    }
-    fun onImageClick(view: View) {
-        when (view.id) {
-            R.id.book_button -> {
-                val likeBookImageView = requireView().findViewById<ImageView>(R.id.like_book)
-                val disBookImageView = requireView().findViewById<ImageView>(R.id.dis_book)
+        binding.bookButton.setOnClickListener {
+            val likeBookImageView = requireView().findViewById<ImageView>(R.id.like_book)
+            val disBookImageView = requireView().findViewById<ImageView>(R.id.dis_book)
 
-                likeBookImageView.visibility = View.VISIBLE
-                disBookImageView.visibility = View.VISIBLE
-            }
-            R.id.like, R.id.dislike, R.id.like_book, R.id.dis_book -> {
-                // send
-            }
+            likeBookImageView.visibility = View.VISIBLE
+            disBookImageView.visibility = View.VISIBLE
         }
+        val sendListener = OnClickListener {
+            // send
+        }
+        binding.like.setOnClickListener(sendListener)
+        binding.dislike.setOnClickListener(sendListener)
+        binding.likeBook.setOnClickListener(sendListener)
+        binding.disBook.setOnClickListener(sendListener)
+        return root
     }
 }
